3a3a31508777157267abc5acdb917f8372f8aeec
Vijaya Malla <mallabiju@gmail.com>
2024-06-20 07:48:28 +0545

    CRUD MVC Empty app with ADO.NET

--- B_Class_Object/b_class_object_data_members/obj/Debug/net8.0/b_class_object_data_members.AssemblyInfo.cs
+++ B_Class_Object/b_class_object_data_members/obj/Debug/net8.0/b_class_object_data_members.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("b_class_object_data_members")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a82727c07f528bd318335f0c69894b92740357")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6190a505aef5841518be98bff929b2b0da1d072")]
 [assembly: System.Reflection.AssemblyProductAttribute("b_class_object_data_members")]
 [assembly: System.Reflection.AssemblyTitleAttribute("b_class_object_data_members")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
